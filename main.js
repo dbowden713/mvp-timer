64d1d3f7fc17aa29efc8fa530f6938b288051c90
Daniel Bowden <dbowden713@gmail.com>
2020-11-18 12:25:19 -0600

    Bump dependencies and enable support for newer electron versions.

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
 const { app, BrowserWindow } = require("electron");
-const path = require('path');
+const path = require("path");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
 function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({ 
-    width: 350, 
-    height: 650, 
-    icon: path.join(__dirname, '/src/icon.png'),
-    frame: false,
-    backgroundColor: '#222',
-    resizable: true,
-    alwaysOnTop: true
-  });
-  
-  //win.setAlwaysOnTop(true, "floating", 1);
+	// Create the browser window.
+	win = new BrowserWindow({
+		width: 350,
+		height: 650,
+		icon: path.join(__dirname, "/src/icon.png"),
+		frame: false,
+		backgroundColor: "#222",
+		resizable: true,
+		alwaysOnTop: true,
+		webPreferences: {
+			nodeIntegration: true,
+			enableRemoteModule: true,
+		},
+	});
 
-  // and load the index.html of the app.
-  win.loadFile("./src/index.html");
+	//win.setAlwaysOnTop(true, "floating", 1);
 
-  //win.webContents.on("devtools-opened", () => { win.webContents.closeDevTools(); });
+	// and load the index.html of the app.
+	win.loadFile("./src/index.html");
 
-  // Open the DevTools.
-  //win.webContents.openDevTools();
+	//win.webContents.on("devtools-opened", () => { win.webContents.closeDevTools(); });
 
-  // Emitted when the window is closed.
-  win.on("closed", () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
+	// Open the DevTools.
+	//win.webContents.openDevTools();
+
+	// Emitted when the window is closed.
+	win.on("closed", () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		win = null;
+	});
 }
 
 // This method will be called when Electron has finished
@@ -43,19 +47,19 @@ app.on("ready", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
 app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-  }
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (win === null) {
+		createWindow();
+	}
 });
 
 // In this file you can include the rest of your app's specific main process
